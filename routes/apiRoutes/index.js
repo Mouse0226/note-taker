cc6d021796df0508a9bf61be5fc72272cadae863
Mouse0226 <mouse1987.ml@gmail.com>
2022-03-03 15:38:17 -0600

    Fixed existing bugs

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 // add const value for imported functions here
-const { db } = require('../../Develop/db/db');
+const notes = require('../../Develop/db/db.json');
 
 router.get('/notes', (req, res) => {
-    let results = db;
+    let results = notes;
     res.json(results);
 });
 
