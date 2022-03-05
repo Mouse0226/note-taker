2888cce09f7e3f8effb802acb677baed51a54870
Mouse0226 <mouse1987.ml@gmail.com>
2022-03-05 17:50:50 -0600

    Resolved folder structure

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,10 +1,22 @@
 const router = require('express').Router();
-// add const value for imported functions here
+const { addNewNote, removeNote } = require('../../lib/notes');
 const notes = require('../../Develop/db/db.json');
 
 router.get('/notes', (req, res) => {
     let results = notes;
     res.json(results);
 });
+
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+
+    const note = addNewNote(req.body, notes)
+    res.json(note);
+});
+
+router.delete('/notes/:id', (req, res) => {    
+    const newNotes = removeNote(req.params.id, notes)
+    res.json(newNotes);
+})
 
 module.exports = router;
